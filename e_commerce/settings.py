9f6231c677e64d4b886b7f1d1db2a4bffb73afa8
Mino_1000 <minoco@hotmail.com>
2019-03-04 08:55:59 -0300

    mudanças no import about_page, contact_page e no urlpatterns about page e contact page

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     	
 ]
 
 MIDDLEWARE = [
@@ -96,9 +97,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -111,3 +112,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    'estaticos',
+
+]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = 'media'
+
+
+
--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -2,12 +2,13 @@
 
 """
 
-from django.contrib import admin
 from django.urls import path
-from .views import home_page
+from .views import home_page, about_page, contact_page
+from django.contrib import admin
 
 urlpatterns = [
-    path('', home_page()),
+    path('', home_page),
+    path('about/', about_page),
+	path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
-
